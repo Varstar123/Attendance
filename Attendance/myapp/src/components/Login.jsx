63307f7b1630e28cbaf8a48d74bf4c453d542c90
Varstar123 <158854610+Varstar123@users.noreply.github.com>
2025-01-21 01:10:32 +0530

    Login.jsx

--- Attendance/myapp/src/components/Login.jsx
+++ Attendance/myapp/src/components/Login.jsx
@@ -1,13 +1,26 @@
-import { Button, TextField } from '@mui/material'
+import { Box, Button, Paper, TextField, Typography } from '@mui/material'
 import React from 'react'
+import './Login.css'
 
 const Login = () => {
   return (
-    <div>
-        <h1>Login Page</h1><br /><br />
-        <TextField variant='standard' label='username'></TextField><br /><br />
-        <TextField variant='standard' label='password'></TextField><br /><br />
-        <Button variant='contained'>Submit</Button>
+    <div backgroundColor='blue'>
+      <br /><br /><br /><br />
+        <Box className = 'papbox'>
+          <Paper className='pap' sx={{borderRadius: 8,backgroundColor:'white',opacity: '0.8', transition: 'all 0.3s ease-in-out','&:hover':{backgroundColor: 'white',opacity: '0.9',transform: 'scale(1.05)'}}}>
+            <div className = 'boxhead'>
+              <Typography variant='poster'>
+                <h2>LOGIN PAGE</h2>
+              </Typography>
+            </div>
+            <br /><br /><br />
+            <div className='boxcont'>
+              <TextField variant='outlined' label='Username' sx={{'& .MuiOutlinedInput-root': {borderRadius: 4}}}></TextField><br /><br />
+              <TextField variant='outlined' label='Password' sx={{'& .MuiOutlinedInput-root': {borderRadius: 4}}}></TextField><br /><br />
+              <Button variant='contained' sx={{borderRadius: 2, backgroundColor: 'black', transition: 'all 0.3s ease-in-out','&:hover':{backgroundColor: 'white', color:'black', transform: 'scale(1.1)'}}}>Submit</Button>
+            </div>
+          </Paper>
+        </Box>
     </div>
   )
 }
