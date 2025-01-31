8c7437549eccdb6a4edf6091139a8ac21745e59f
Varstar123 <158854610+Varstar123@users.noreply.github.com>
2025-01-31 21:37:44 +0530

    Add files via upload

--- Attendance/myapp/src/components/Login.jsx
+++ Attendance/myapp/src/components/Login.jsx
@@ -1,13 +1,19 @@
 import { Box, Button, Paper, TextField, Typography } from '@mui/material'
 import React from 'react'
 import './Login.css'
+import { Link, useNavigate } from 'react-router-dom'
+
 
 const Login = () => {
+  const navigate = useNavigate();
+  navigate("/calculator");
+
+
   return (
     <div backgroundColor='blue'>
       <br /><br /><br /><br />
         <Box className = 'papbox'>
-          <Paper className='pap' sx={{borderRadius: 8,backgroundColor:'white',opacity: '0.8', transition: 'all 0.3s ease-in-out','&:hover':{backgroundColor: 'white',opacity: '0.9',transform: 'scale(1.05)'}}}>
+          <Paper className='pap' sx={{borderRadius: 8,backgroundColor:'white',opacity: '0.8', transition: 'all 0.3s ease-in-out','&:hover':{backgroundColor: 'white',opacity: '1',transform: 'scale(1.05)'}}}>
             <div className = 'boxhead'>
               <Typography variant='poster'>
                 <h2>LOGIN PAGE</h2>
@@ -16,8 +22,10 @@ const Login = () => {
             <br /><br /><br />
             <div className='boxcont'>
               <TextField variant='outlined' label='Username' sx={{'& .MuiOutlinedInput-root': {borderRadius: 4}}}></TextField><br /><br />
-              <TextField variant='outlined' label='Password' sx={{'& .MuiOutlinedInput-root': {borderRadius: 4}}}></TextField><br /><br />
-              <Button variant='contained' sx={{borderRadius: 2, backgroundColor: 'black', transition: 'all 0.3s ease-in-out','&:hover':{backgroundColor: 'white', color:'black', transform: 'scale(1.1)'}}}>Submit</Button>
+              <TextField variant='outlined' type='password' label='Password' sx={{'& .MuiOutlinedInput-root': {borderRadius: 4}}}></TextField><br /><br />
+              <Link>
+                <Button variant='contained' sx={{borderRadius: 2, backgroundColor: 'black', transition: 'all 0.3s ease-in-out','&:hover':{backgroundColor: 'white', color:'black', transform: 'scale(1.1)'}}}>Submit</Button>
+              </Link>
             </div>
           </Paper>
         </Box>
